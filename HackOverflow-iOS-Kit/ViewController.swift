5dc3eadcba3e7ac70919e2d5d34aec5cc06f90e7
carolineh101 <cho19@stanford.edu>
2017-03-06 11:18:40 -0800

    Initial Commit

---
+++ HackOverflow-iOS-Kit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackOverflow-iOS-Kit
+//
+//  Created by Caroline Ho on 3/6/17.
+//  Copyright © 2017 HackOverflow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
